40bef1fcbe5cd745024488e272fe9a288b193c09
DGFinder <hayden.george.hamilton@gmail.com>
2025-07-02 14:41:44 +0800

    Update map tile layer in MapView to use CARTO basemaps, enhancing visual quality and attribution clarity. Maintain OpenStreetMap attribution for compliance.

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -157,8 +157,8 @@ export default function MapView() {
             style={{ height: '100%', width: '100%' }}
           >
             <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
             {/* Tank markers with coordinate data */}
             {filteredTanks?.map((tank) => (
